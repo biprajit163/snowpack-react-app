5778eb7d3393e4700e2ce62f5b81f82efb15d8cb
Biprajit Majumder <biprajit163@gmail.com>
2021-04-23 01:07:49 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/App.css';
+
+function App(props) {
+    return (
+        <div className="App">
+            <h1>Hello Custom React App</h1>
+        </div>
+    );
+}
+
+export default App;
