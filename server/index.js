53b6eddbb1214b1a8908fa64f493921f5025a989
Biprajit Majumder <biprajit163@gmail.com>
2021-04-24 02:48:20 -0400

    commit

--- server/index.js
+++ server/index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
+const http = require('http').createServer();
+// const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello from socket.io</h1>');
+
+const io = require('socket.io')(http, {
+    cors: { origin: "*" }
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('message', (message) => {
+        console.log(message);
+        io.emit('message', `${socket.id.substr(0,2)} said ${message}`);
+    });
 });
+
+
+http.listen(8080, () => console.log(`listening on http://localhost:8080`));
+
+
+/* ------------------------------------------------------------------------------- */
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + './index.html');
+// });
+
+// server.listen(3000, () => {
+//     console.log('listening on *:3000');
+// });
